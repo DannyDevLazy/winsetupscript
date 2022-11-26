6a342ebb494ee4514369233196ec43d9a2b6f5df
DannyDevLazy <dannydevlazy@gmail.com>
2022-11-26 13:32:18 +0100

    fix comment

--- win_setup.ps1
+++ win_setup.ps1
@@ -5,7 +5,7 @@ powershell choco feature enable -n allowGlobalConfirmation
 $chocoversion = powershell choco -v
 Write-Output "Chocolatey Version $chocoversion installed"
 
-Install prerequisites
+#Install prerequisites
 irm "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"-o "./Microsoft.VCLibs.x64.14.00.Desktop.appx"
 irm "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"-o "./Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 irm "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/b0a0692da1034339b76dce1c298a1e42_License1.xml"-o "./b0a0692da1034339b76dce1c298a1e42_License1.xml"
