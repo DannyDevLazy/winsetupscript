ae82828ed0eb807b8105afdd2ee208af99844f45
DannyDevLazy <dannydevlazy@gmail.com>
2022-11-26 11:48:57 +0100

    Streamline commands

--- win_setup.ps1
+++ win_setup.ps1
@@ -1,10 +1,6 @@
 # Chocolatey install
-$testchoco = powershell choco -v
-if (-not($testchoco)){
-    Write-Output "Seems Chocolatey is not installed, installing now"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadStringo('https://chocolatey.org/install.ps1'))
-    powershell choco feature enable -n allowGlobalConfirmation
-}
-else {
-    Write-Output "Chocolatey Version $testchoco is already installed"
-}
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadStringo('https://chocolatey.org/install.ps1'))
+powershell choco feature enable -n allowGlobalConfirmation
+$chocoversion = powershell choco -v
+Write-Output "Chocolatey Version $chocoversion installed"
