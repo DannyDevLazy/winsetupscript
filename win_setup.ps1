f5df41d1865819a51444d19ef6af8755c82295eb
DannyDevLazy <dannydevlazy@gmail.com>
2022-11-26 11:40:38 +0100

    Add choco installation to the script

--- win_setup.ps1
+++ win_setup.ps1
@@ -1,1 +1,10 @@
-
+# Chocolatey install
+$testchoco = powershell choco -v
+if (-not($testchoco)){
+    Write-Output "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadStringo('https://chocolatey.org/install.ps1'))
+    powershell choco feature enable -n allowGlobalConfirmation
+}
+else {
+    Write-Output "Chocolatey Version $testchoco is already installed"
+}
