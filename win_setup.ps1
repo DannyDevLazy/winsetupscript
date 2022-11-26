2dbe52546d7d162e00a30d515b170f5aa83d2350
DannyDevLazy <dannydevlazy@gmail.com>
2022-11-26 12:22:35 +0100

    Add essential apps

--- win_setup.ps1
+++ win_setup.ps1
@@ -1,6 +1,14 @@
 # Chocolatey install
 Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadStringo('https://chocolatey.org/install.ps1'))
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 powershell choco feature enable -n allowGlobalConfirmation
 $chocoversion = powershell choco -v
 Write-Output "Chocolatey Version $chocoversion installed"
+
+#Install essentials
+if (-not($chocoversion)){
+    Write-Output "Chocolatey not installed!"
+} else {
+    choco install brave
+    choco install steam
+}
